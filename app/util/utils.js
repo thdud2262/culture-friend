4d25d9f75227936629eacaaa6cb5731f8f757b0e
thdud <thgml1444@naver.com>
2023-07-29 23:07:16 +0900

    [ home-ADD ] 메인 공연리스트의 캐러셀을 구현하라 - 공연 종료일이 20일 남은 리스트를 메인으로 보여주어 캐러셀을 구현하였다 - 리스트 필터링 함수를 utils로 작성하였다 - auto-play기능을 구현하여 2.5초마다 공연정보가 넘어가도록 하였다 - D-day 20일 공연 리스트 페이지를 만들까 고민이다...

--- app/util/utils.js
+++ app/util/utils.js
@@ -13,13 +13,28 @@ export const API_SortFunc = (data) => {
   return data;
 };
 
-// 지난 행사 데이터를 제외한 리스트 필터 함수
+// 지난 공연 데이터를 제외하는 필터링 함수
 export const API_FilterFunc = (data) => {
   const curDate = new Date().toISOString().split("T")[0];
+
   const filterData = data.filter((item) => {
     const dateRange = item.DATE.split("~");
     const endDate = new Date(dateRange[1]).toISOString().split("T")[0];
     return endDate >= curDate;
   });
   return filterData;
+};
+
+// 20일 내로 끝나는 공연 리스트 필터링 함수
+export const API_WeekendFilterFunc = (data) => {
+  const today = new Date();
+  const nextWeek = new Date(today);
+  nextWeek.setDate(today.getDate() + 20);
+  const nextWeekDay = nextWeek.toISOString().split('T')[0]
+
+  const weekendFilterData = data.filter((item) => {
+    const endDate = item.END_DATE.split(" ")[0];
+    return endDate < nextWeekDay;
+  });
+  return weekendFilterData;
 };
