0ef2fd544aef148e6933e05f68203f6dd5ad7801
thdud <thgml1444@naver.com>
2023-08-03 23:25:10 +0900

    [ calendar-UPDATE ] calendar-view에 API 데이터를 연동시켜라 - 달력 사이즈에 맞게 API데이터를 CSS로 조절하였다 - array.from으로 만든 calendar-date와 API-DATE와 형식을 맞춰 해당 날짜의 공연이 보일 수 있도록 하였다

--- app/cultureCalendar/Calendar.jsx
+++ app/cultureCalendar/Calendar.jsx
@@ -1,44 +1,43 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import styles from "./cultureCalendar.module.css";
+import Link from "next/link";
+// util함수 import
+import { API_SortFunc, serviceKey } from "../util/utils";
+import { useMonthNavigation } from "../util/useMonthNavigator";
 
 export default function Calendar() {
-  const date = new Date();
-  const [curDate, setCurDate] = useState({
-    year: date.getFullYear(),
-    month: date.getMonth(),
-    date: date.getDate(),
-  });
-  
+  const todayDate = new Date();
+  const [apiData, setApiData] = useState([]);
+  const { curDate, handlePrevMonth, handleNextMonth } =
+    useMonthNavigation(todayDate);
+  const urlDate = `${curDate.year}-${String(curDate.month + 1).padStart(
+    2,
+    "0"
+  )}`;
+  const url = `http://openapi.seoul.go.kr:8088/${serviceKey}/json/culturalEventInfo/1/50/ / /${urlDate}`;
+  // API호출
+  useEffect(() => {
+    fetch(url, {
+      method: "GET",
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        if (!result.culturalEventInfo) return null;
+        const lists = result.culturalEventInfo.row;
+        const dataSortList = API_SortFunc(lists);
+        setApiData(dataSortList);
+      });
+  }, [urlDate]);
+
   const numberDate = new Date(curDate.year, curDate.month + 1, 0).getDate();
+  // 유사배열객체 생성, lnegth길이만큼 배열생성 / 두번째 인수의 반환값으로 구성된 배열반환
   const numbersArray = Array.from(
     { length: numberDate },
-    (_, index) => index + 1
-  );
+    (_, index) => (index + 1).toString().padStart(2, '0')
+    );
   const gridColumnStart = new Date(curDate.year, curDate.month, 1).getDay() + 1;
   const firstGridDateStyle = { gridColumnStart: gridColumnStart };
-
-  const handlePrevMonth = () => {
-    setCurDate((state) => {
-      const prevMonth = state.month - 1;
-      const prevYear = state.year;
-      if(prevMonth < 0){
-        return {...state, month:11, year:prevYear-1}
-      }
-      return { ...state, month: prevMonth };
-    });
-  };
-
-  const handleNextMonth = () => {
-    setCurDate((state)=>{
-      const nextMonth = state.month + 1
-      const nextYear = state.year
-      if(nextMonth > 11){
-        return {...state, month: 0, year: nextYear+1}
-      }
-      return { ...state, month: nextMonth }
-    })
-  };
 
   const isSunday = (year, month, day) => {
     return new Date(year, month, day).getDay() === 0;
@@ -52,7 +51,9 @@ export default function Calendar() {
     <div className={styles.calendarBox}>
       <div className={styles.calendarMonth}>
         <button onClick={handlePrevMonth}> {" < "} </button>
-        <p>{curDate.year}년 {curDate.month+1}월</p>
+        <p>
+          {curDate.year}년 {curDate.month + 1}월
+        </p>
         <button onClick={handleNextMonth}> {" > "} </button>
       </div>
       <div className={styles.days}>
@@ -70,28 +71,70 @@ export default function Calendar() {
           if (isSunday(curDate.year, curDate.month, ele)) {
             cellStyle.color = "red";
           }
-          
           if (isSaturday(curDate.year, curDate.month, ele)) {
             cellStyle.color = "blue";
           }
 
-          return ele === 1 ? (
-            <p
-              key={1}
-              className={styles.date}
-              style={{ ...firstGridDateStyle, ...cellStyle }}
-            >
-              {1}
-            </p>
-          ) : (
-            <p
-              key={ele}
-              className={styles.date}
-              style={ cellStyle }
-            >
-              {ele}
-            </p>
-          );
+          // 해당 일의 공연 필터링 함수
+          const API_calendarDataFilter = apiData.filter((data)=>{
+            const currentDate= data.STRTDATE.split(' ')[0].split('-')[2]
+            return currentDate == ele
+          })
+          
+            return ele == '01' ? (
+              <div
+                key={ele}
+                className={styles.dateBox}
+                style={{ ...firstGridDateStyle }}
+              >
+                <p className={styles.date} style={cellStyle}>
+                  {ele}
+                </p>
+                <div className={styles.dateCultureList}>
+                <ul>
+                  {API_calendarDataFilter.map((data,idx)=>{
+                    if(idx < 5){
+                      return (
+                        <>
+                        <li key={idx} className={styles.calendarDataList}>
+                          <Link href={data.ORG_LINK} target="_blank">
+                          {data.TITLE}
+                          </Link>
+                        </li>
+                      </>
+                      )
+                    }
+                    })}
+                  {API_calendarDataFilter.length > 4 ? <button>더보기</button> : ""}
+                  </ul>
+                </div>
+              </div>
+            ) : (
+              <div key={ele} className={styles.dateBox}>
+                <p className={styles.date} style={cellStyle}>
+                  {ele}
+                </p>
+                <div className={styles.dateCultureList}> 
+                  <ul>
+                  {API_calendarDataFilter.map((data,idx)=>{
+                    if(idx < 5){
+                      return (
+                        <>
+                        <li key={idx} className={styles.calendarDataList}>
+                          <Link href={data.ORG_LINK} target="_blank">
+                          {data.TITLE}
+                          </Link>
+                        </li>
+                      </>
+                      )
+                    }
+                    })}
+                  {API_calendarDataFilter.length > 4 ? <button>더보기</button> : ""}
+                  </ul>
+                </div>
+              </div>
+            );
+        
         })}
       </div>
     </div>
