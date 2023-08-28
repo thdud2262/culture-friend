7ea82b53bf75f9ea06503f75dcb3aaae079dcfeb
thdud <thgml1444@naver.com>
2023-08-28 17:21:42 +0900

    [ code-delete ] 사용하지 않는 코드 삭제

--- app/components/homeComp/HomeList.jsx
+++ app/components/homeComp/HomeList.jsx
@@ -7,9 +7,6 @@ export default function HomeList({ subTitle, codename }) {
     <div className={styles.cultureListBox}>
       <div className={styles.subTitleBox}>
         <h1 className={styles.subTitle}>{subTitle}</h1>
-        <Link href="/cultureList" className={styles.subLink}>
-          상세페이지 이동
-        </Link>
       </div>
       <HomeListItem codename={codename} />
     </div>
