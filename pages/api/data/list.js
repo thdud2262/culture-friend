bed0af1b0066b54b45c83dfd00de67fc448428f5
thdud <thgml1444@naver.com>
2023-08-30 19:58:19 +0900

    [ API-EDIT ] 서버에서 데이터 요청을 세분화하라 - list를 다 받아오는 것이 아니라, filter와 sort를 거쳐 클라이언트에서 받아서 사용할 수 있도록 하였다

--- pages/api/data/list.js
+++ pages/api/data/list.js
@@ -1,22 +1,27 @@
+import { API_FilterFunc, API_SortFunc } from "@/app/util/utils";
 import fetch from "node-fetch";
 
 export default async function handler(req, res) {
   const { codename, serviceKey, type, urlDate } = req.body;
-  console.log(type);
-
   const API_URL =
     type == "cultureList"
-      ? `http://openapi.seoul.go.kr:8088/${serviceKey}/json/culturalEventInfo/1/150/${codename}/ /${urlDate}`
-      : `http://openapi.seoul.go.kr:8088/${serviceKey}/json/culturalEventInfo/1/150/ / /${urlDate}`;
+      ? `http://openapi.seoul.go.kr:8088/${serviceKey}/json/culturalEventInfo/1/100/${codename}/ /${urlDate}`
+      : `http://openapi.seoul.go.kr:8088/${serviceKey}/json/culturalEventInfo/1/100/ / /${urlDate}`;
 
   await fetch(API_URL)
     .then((response) => {
       if (!response.ok) {
-        throw new Error("API request failed");
+        throw new Error("API request failed --- fetch 요청실패");
       }
       return response.json();
     })
     .then((result) => {
-      return res.status(200).json(result);
+      const lists = result.culturalEventInfo.row;
+      const listCopy = [...lists];
+
+      const dataSortList = API_SortFunc(listCopy);
+      const filterList = API_FilterFunc(dataSortList);
+
+      return res.status(200).json(filterList);
     });
 }
